7fbd5e7c9f2c2d7a5f18b1f031bf654445b9830d
Abbas196 <abbasraza196@gmail.com>
2023-10-13 13:11:55 -0500

    backend code fixed

--- backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthenticationController.java
+++ backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthenticationController.java
@@ -8,14 +8,12 @@ import com.cricmate.CricketRecordsApplication.dao.Response.JwtAuthenticationResp
 import com.cricmate.CricketRecordsApplication.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@CrossOrigin
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
