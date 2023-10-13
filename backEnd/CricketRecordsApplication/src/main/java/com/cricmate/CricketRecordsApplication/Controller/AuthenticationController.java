c913bc02b766f2e76051e545ebb43481dd0e1f57
Abbas196 <abbasraza196@gmail.com>
2023-10-13 13:25:45 -0500

    cors origin issue resolved

--- backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthenticationController.java
+++ backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthenticationController.java
@@ -9,20 +9,24 @@ import com.cricmate.CricketRecordsApplication.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-@CrossOrigin
 public class AuthenticationController {
+    @Value("${server.port}")
+    private String serverPort;
     private final AuthenticationService authenticationService;
 
     @PostMapping("/user/signup")
+    @CrossOrigin(origins = "http://localhost:${server.port}")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request){
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
     @PostMapping("/user/signin")
+    @CrossOrigin(origins = "http://localhost:${server.port}")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request){
         return ResponseEntity.ok(authenticationService.signin(request));
     }
--- backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthorizationController.java
+++ backEnd/CricketRecordsApplication/src/main/java/com/cricmate/CricketRecordsApplication/Controller/AuthorizationController.java
@@ -1,7 +1,9 @@
 package com.cricmate.CricketRecordsApplication.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/resource")
 @RequiredArgsConstructor
 public class AuthorizationController {
+    @Value("${server.port}")
+    private String serverPort;
     @GetMapping
+    @CrossOrigin(origins = "http://localhost:${server.port}")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Here is your resource");
     }
